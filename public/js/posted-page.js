35cbf255e144fa1d9373d181b1e19130116857f0
yongmin kim <kym930722@gmail.com>
2020-11-17 21:48:42 +0900

    scrap, like

--- public/js/posted-page.js
+++ public/js/posted-page.js
@@ -1,5 +1,7 @@
 // status
 let posts;
+let scrap;
+let like;
 
 // doms
 const $postedPageWritterInfo = document.querySelector('.posted-page-writter-info');
@@ -9,11 +11,21 @@ const $postedPageTime = document.querySelector('.posted-page-time');
 const $postedPageContent = document.querySelector('.posted-page-content');
 const $postedpageAlbum = document.querySelector('.posted-page-album');
 const $postedpageYtTitle = document.querySelector('.posted-page-youtube-title');
+const $postedPageStatus = document.querySelector('.posted-page-status');
+const $postedPageLikeBtn = document.querySelector('.posted-page-likeBtn');
+const $postedPageScrapBtn = document.querySelector('.posted-page-scrapBtn');
 
 // functions
 const request = {
   get(url) {
     return fetch(url);
+  },
+  patch(url, payload) {
+    return fetch(url, {
+      method: 'PATCH',
+      headers: { 'content-Type': 'application/json' },
+      body: JSON.stringify(payload)
+    });
   }
 };
 
@@ -24,6 +36,15 @@ const renderWritterInfo = post => {
   $postedPageContent.textContent = post.content;
   $postedpageAlbum.style.backgroundImage = `url('${post.music.thumbnail}')`;
   $postedpageYtTitle.textContent = post.music.title;
+  $postedPageScrapBtn.nextElementSibling.textContent = post.scrap.length;
+  $postedPageLikeBtn.nextElementSibling.textContent = post.like.length;
+};
+
+const checkPressedLikeBtn = () => {
+  
+};
+const checkPressedScrapBtn = () => {
+
 };
 
 // Events
@@ -39,4 +60,39 @@ window.onload = () => {
 
 $postedpageAlbum.onmouseenter = e => {
   console.log(e.target);
+};
+
+$postedPageStatus.onclick = e => {
+  if (!e.target.matches('button')) return;
+
+  console.log(e.target);
+  if (e.target.classList[1] === 'scrap') {
+    (async () => {
+      const res = await request.get(`/posts/${sessionStorage.getItem('post-id')}`);
+      const post = await res.json();
+      scrap = post.scrap;
+      console.log(scrap);
+      scrap.push(sessionStorage.getItem('post-id'));
+      
+      const res2 = await request.patch(`/posts/${sessionStorage.getItem('post-id')}`, { scrap });
+      //scrap의 프로퍼티값으로 현재 로그인된 사람 아이디 넣기
+      const post2 = await res2.json();
+      console.log(post2.scrap.length);
+      $postedPageScrapBtn.nextElementSibling.textContent = post.scrap.length;
+    })();
+  } else if (e.target.classList[1] === 'like') {
+    (async () => {
+      const res = await request.get(`/posts/${sessionStorage.getItem('post-id')}`);
+      const post = await res.json();
+      like = post.like;
+      console.log(like);
+      like.push(sessionStorage.getItem('post-id'));
+      
+      const res2 = await request.patch(`/posts/${sessionStorage.getItem('post-id')}`, { like });
+      //like의 프로퍼티 값으로 현재 로그인된 사람 아이디 넣기.
+      const post2 = await res2.json();
+      console.log(post2.like.length);
+      $postedPageLikeBtn.nextElementSibling.textContent = post.like.length;
+    })();
+  }
 };
