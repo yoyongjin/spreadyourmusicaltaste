d92642c117e44646762ccaafd9bc2d69ce5146be
yongmin kim <kym930722@gmail.com>
2020-11-19 13:33:02 +0900

    update

--- public/js/main-page.js
+++ public/js/main-page.js
@@ -12,7 +12,7 @@ const $icon = document.querySelector('.scroll-icon');
 const $sortBtn = document.querySelector('.sort');
 const $orderPanel = document.querySelector('.order-panel');
 
-// functions
+// 포스팅 노래 렌더 함수
 const renderPost = () => {
   $mainBoard.innerHTML = '';
   $mainBoard.innerHTML += posts.map(post => `<li class="main-post ${post.id}">
@@ -26,11 +26,11 @@ const renderPost = () => {
       <img class="record-needle" src="https://static.thenounproject.com/png/1892806-200.png" alt="바늘" />
   </li>`).join('');
 };
-
+// 상단에 로그인 유저 아이디 출력
 const displayUserName = userInfo => {
   $userName.textContent = `${userInfo.nickname}님 안녕하세요.`;
 };
-
+// 유튜브 api에서 가져온 썸네일 렌더해주는 함수
 const applyThumbnail = () => {
   const items = [...document.querySelectorAll('.main-post-image')];
   items.forEach((item, i) => {
@@ -44,7 +44,7 @@ const request = {
     return fetch(url);
   }
 };
-
+// 로딩화면
 const displayLoading = () => {
   const $imgContainer = document.createElement('div');
   const $img = document.createElement('img');
@@ -55,7 +55,7 @@ const displayLoading = () => {
   $imgContainer.appendChild($img);
   $mainMain.appendChild($imgContainer);
 };
-
+// 다음 데이터 확인해서 출력
 const loadNextPosts = () => {
   const { scrollY } = window;
   if (scrollY + document.body.getBoundingClientRect().height >= $mainMain.scrollHeight + 50 && !isLastPage) {
@@ -67,7 +67,7 @@ const loadNextPosts = () => {
       const _posts_length = await res_length.json();
       
       const pageNum = Math.ceil(_posts_length.length / 6);
-      const res = await request.get(`/posts?_page=${pageCount + 1}&_limit=6&_sort=${sortBy}&_order=desc`);
+      const res = await request.get(`/posts?_page=${pageCount + 1}&_limit=6&_sort=${sortBy},id&_order=desc,desc`);
       const _posts = await res.json();
       pageCount++;
       if (_posts.length < 6 || pageNum === pageCount) { // 마지막페이지인 경우
@@ -86,7 +86,7 @@ const loadNextPosts = () => {
     }, 300);
   }
 };
-
+// 스크롤 상단으로 옮겨주는 버튼 출력
 const displayBtn = () => {
   const { scrollY } = window;
   $icon.style.display = scrollY >= 450 ? 'block' : 'none';
@@ -99,14 +99,14 @@ window.onload = () => {
       : orderState === 'like' ? 'like.length' : 'scrap.length';
     userInfo = JSON.parse(sessionStorage.getItem('user'));
     displayUserName(userInfo);
-    const res = await request.get(`/posts?_page=1&_limit=6&_sort=${sortBy}&_order=desc`);
+    const res = await request.get(`/posts?_page=1&_limit=6&_sort=${sortBy},id&_order=desc,desc`);
     posts = await res.json();
     renderPost();
     applyThumbnail();
     displayBtn();
   })();
 };
-
+// 스크롤 이벤트 스로틀
 document.onscroll = _.throttle(() => {
   loadNextPosts();
   displayBtn();
@@ -147,7 +147,7 @@ $orderPanel.onclick = e => {
     displayUserName(userInfo);
     pageCount = 1;
     isLastPage = false;
-    const res = await request.get(`/posts?_page=1&_limit=6&_sort=${sortBy}&_order=desc`);
+    const res = await request.get(`/posts?_page=1&_limit=6&_sort=${sortBy},id&_order=desc,desc`);
     posts = await res.json();
     renderPost();
     applyThumbnail();
