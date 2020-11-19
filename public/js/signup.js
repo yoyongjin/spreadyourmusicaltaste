b2517d1115aa7f5d4d7929321f9913587f0f9aac
dongchan-K <ehdcks1028@gmail.com>
2020-11-19 11:50:55 +0900

    feat: 마이 포스트 페이지 마이 스크랩 페이지 수정

--- public/js/signup.js
+++ public/js/signup.js
@@ -12,21 +12,23 @@ const $nickNameError = document.querySelector('.nickname-error');
 
 const $userInfo = document.querySelectorAll('.signup-items input');
 
-
-// Event Handler
+// 정규 표현식
+const idReg = /^[A-Za-z0-9+]{6,12}$/;
+const pwReg = /^[A-Za-z0-9+]{8,12}$/;
 
 // 아이디 입력창 이벤트
 $inputId.onblur = async () => {
-  const idReg = /^[A-Za-z0-9+]{6,12}$/;
 
   $idError.textContent = '';
 
   if (!$inputId.value.trim()) {
     $idError.textContent = '필수 입력 항목입니다.';
+    $inputId.style.boxShadow = '0 0 2pt 4pt red';
     return;
   }
   if (!idReg.test($inputId.value)) {
     $idError.textContent = '6~12자의 영문자, 숫자만 사용가능합니다.';
+    $inputId.style.boxShadow = '0 0 2pt 4pt red';
     return;
   }
 
@@ -37,29 +39,31 @@ $inputId.onblur = async () => {
 
     if (checkId) {
       $idError.textContent = '이미 존재하는 아이디입니다.';
+      $inputId.style.boxShadow = '0 0 2pt 4pt red';
       return;
     }
+    $inputId.style.boxShadow = '0 0 2pt 4pt yellowgreen';
   } catch (err) {
     console.error(err);
   }
 };
 
-
 // 비밀번호 입력 이벤트
 $inputPw.onblur = () => {
-
-  const pwReg = /^[A-Za-z0-9+]{8,12}$/;
 
   $pwError.textContent = '';
 
   if (!$inputPw.value.trim()) {
     $pwError.textContent = '필수 입력 항목입니다.';
+    $inputPw.style.boxShadow = '0 0 2pt 4pt red';
     return;
   }
   if (!pwReg.test($inputPw.value)) {
     $pwError.textContent = '8~12자의 영문자, 숫자만 사용가능합니다.';
-    // return;
+    $inputPw.style.boxShadow = '0 0 2pt 4pt red';
+    return;
   }
+  $inputPw.style.boxShadow = '0 0 2pt 4pt yellowgreen';
 };
 
 // 비밀번호 확인 이벤트
@@ -68,21 +72,25 @@ $ckPassword.onblur = () => {
 
   if (!$ckPassword.value.trim()) {
     $checkPwError.textContent = '필수 입력 항목입니다.';
+    $ckPassword.style.boxShadow = '0 0 2pt 4pt red';
     return;
   }
   if ($ckPassword.value !== $inputPw.value) {
     $checkPwError.textContent = '비밀번호가 일치하지 않습니다.';
-    // return;
+    $ckPassword.style.boxShadow = '0 0 2pt 4pt red';
+    return;
   }
-
+  $ckPassword.style.boxShadow = '0 0 2pt 4pt yellowgreen';
 };
 
 // 닉네임 확인 이벤트
 $inputNickname.onblur = async () => {
   $nickNameError.textContent = '';
+  const userCheck = [...$userInfo].filter(userInfo => userInfo.value.length);
 
   if (!$inputNickname.value.trim()) {
     $nickNameError.textContent = '필수 입력 항목입니다.';
+    $inputNickname.style.boxShadow = '0 0 2pt 4pt red';
     return;
   }
 
@@ -92,16 +100,25 @@ $inputNickname.onblur = async () => {
     const checkNickname = await users.find(user => user.nickname === $inputNickname.value);
     if (checkNickname) {
       $nickNameError.textContent = '이미 존재하는 닉네임 입니다.';
+      $inputNickname.style.boxShadow = '0 0 2pt 4pt red';
+      $inputNickname.focus();
       return;
     } 
+    $inputNickname.style.boxShadow = '0 0 2pt 4pt yellowgreen';
+    $signupBtn.classList.add('focus-btn');
+    if (userCheck < 4) return;
+    $signupBtn.style.backgroundColor = 'yellowgreen';
   } catch (err) {
     console.error(err);
   }
 };
 
+
 $signupBtn.onmouseenter = () => {
   const userCheck = [...$userInfo].filter(userInfo => userInfo.value.length);
+  $signupBtn.style.backgroundColor = 'red';
   if (userCheck.length < 4) return;
+  $signupBtn.style.backgroundColor = 'yellowgreen';
   $signupBtn.classList.add('focus-btn');
 };
 
